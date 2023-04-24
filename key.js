97e62cc612ad0b3bd940193e5f068961a12269a5
Myreen Ahmed <ahmedmyreen@gmail.com>
2023-04-23 22:44:17 -0400

    final changes made, ready to submit

--- key.js
+++ key.js
@@ -1,1 +1,1 @@
-export const API_KEY = '83c9bffe98be4fb39b22d95d93999870'
+export const API_KEY = "83c9bffe98be4fb39b22d95d93999870";
--- script.js
+++ script.js
@@ -9,32 +9,39 @@ const movieInfo = document.getElementById("movie-info");
 
 getMovieBtn.addEventListener("click", () => {
   const selectedMovieId = movieSelect.value;
- // if (selectedMovieId) {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/movie/${selectedMovieId}?api_key=${API_KEY}&append_to_response=videos`
-         )
-      .then((response) => {
-        console.log(response.data);
-        const movieData = response.data;
-        const trailer = movieData.videos.results.find(
-          (video) => video.type === "Trailer"
-        );
-        
+  axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${selectedMovieId}?api_key=${API_KEY}&append_to_response=videos`
+    )
+    .then((response) => {
+      console.log(response.data);
+      const movieData = response.data;
+      const trailer = movieData.videos.results.find(
+        (video) => video.type === "Trailer"
+      );
 
-        const posterPath = `https://image.tmdb.org/t/p/w500${movieData.poster_path}`;
+      const posterPath = `https://image.tmdb.org/t/p/w500${movieData.poster_path}`;
+      const genreLength = movieData.genres.length;
+      let gnrNames = "";
+      for (const gnr of movieData.genres) {
+        gnrNames = gnrNames + gnr.name + ", ";
+      }
+      // removing extra coma and space from end
+      gnrNames = gnrNames.substring(0, gnrNames.length - 2);
 
-        movieInfo.innerHTML = `
+      movieInfo.innerHTML = `
           <h2>${movieData.title}</h2>
           <p>${movieData.overview}</p>
           <p>Release date: ${movieData.release_date}</p>
           <p>Runtime: ${movieData.runtime} minutes</p>
           <p>Rating: ${movieData.vote_average} (${
-          movieData.vote_count
-        } votes)</p>
+        movieData.vote_count
+      } votes)</p>
         <p>Original Language: ${movieData.original_language}</p>
         <p>Revenue: ${movieData.revenue} USD</p>
         <p>Budget: ${movieData.budget} USD</p>
+        <p>Genres: ${gnrNames} </p>
+        
 
           ${
             trailer
@@ -43,6 +50,5 @@ getMovieBtn.addEventListener("click", () => {
           }
           <img src="${posterPath}" alt="${movieData.title} poster">
         `;
-      });
-  }
-);
+    });
+});
